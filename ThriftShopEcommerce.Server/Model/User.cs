d403c7aaa97aeeaf885f6107dbe0b9d7564b9a8b
LanceRuzel <lanceruzel2@gmail.com>
2025-02-10 20:57:47 +0800

    feat: setup EF Core, Identity, and Swagger
    - Configure EF Core with SQL Server
    - Add authorization and identity services
    - Setup Swagger for API documentation
    - Update Program.cs with necessary configurations
    - Modify launchSettings.json to auto-launch Swagger UI
    - Update project references and add connection string
    - Add ApplicationDbContext and User entity

---
+++ ThriftShopEcommerce.Server/Model/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ThriftShopEcommerce.Server.Model
+{
+    public class User: IdentityUser
+    {
+    }
+}
--- ThriftShopEcommerce.Server/Program.cs
+++ ThriftShopEcommerce.Server/Program.cs
@@ -1,4 +1,9 @@
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using ThriftShopEcommerce.Server.Data;
+using ThriftShopEcommerce.Server.Model;
+
 namespace ThriftShopEcommerce.Server
 {
     public class Program
@@ -7,21 +12,35 @@ namespace ThriftShopEcommerce.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            //Database Service
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<User>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+            //builder.Services.AddOpenApi();
+
+            // Register Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
             app.UseDefaultFiles();
             app.MapStaticAssets();
+            app.MapIdentityApi<User>();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                //app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
